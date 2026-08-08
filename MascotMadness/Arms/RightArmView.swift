541471659213f5a12eb446aa9d2849a281bc6a40
shivikabhan13-lang <shivika.bhan13@rafflesgirlssch.edu.sg>
2026-08-08 15:12:54 +0800

    Attempt 1 for right arm - gradient from red to white and cannot view on my own device due to loading errors

--- MascotMadness/Arms/RightArmView.swift
+++ MascotMadness/Arms/RightArmView.swift
@@ -15,11 +15,15 @@ var rightArm: some BodyPart {
                 Rectangle()
                     .frame(width: 30, height: 200)
                     .foregroundStyle(.red)
+                LinearGradient(
+                    colors: [Color(red: 1.0, green: 0.027, blue: 0.227), .white],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
             }
         }
     }
 }
-
 #Preview(traits: .fixedLayout(width: 200, height: 200)) {
     rightArm
 }
